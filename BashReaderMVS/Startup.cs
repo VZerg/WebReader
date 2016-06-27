6726f309fe0dd7bd784fdf3cad0cb8b4381250a1
Zerg <vetrov.zerg@gmail.com>
2016-06-27 13:42:21 +0600

    First step

---
+++ BashReaderMVS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BashReaderMVS.Startup))]
+namespace BashReaderMVS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
